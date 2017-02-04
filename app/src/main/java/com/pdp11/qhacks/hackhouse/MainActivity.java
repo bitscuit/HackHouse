8fdc9610eb080957e6132f571469887d1e1dd810
NewmanM <m_tanel@live.ca>
2017-02-04 01:04:17 -0500

    Added google authentication

--- app/src/main/java/com/pdp11/qhacks/hackhouse/MainActivity.java
+++ app/src/main/java/com/pdp11/qhacks/hackhouse/MainActivity.java
@@ -2,6 +2,7 @@ package com.pdp11.qhacks.hackhouse;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -15,6 +16,12 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ListView;
 
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,12 +30,23 @@ public class MainActivity extends AppCompatActivity {
     TodoAdapter adapter;
     ListView lView;
 
+    // Firebase database references
+    private DatabaseReference mDatabase;
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private String mUsername;
+    private String mPhotoUrl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initializeFirebase();
 
         todoList = new ArrayList<>();
         adapter = new TodoAdapter(todoList, this);
@@ -75,6 +93,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
     } // end onCreate method
+
+    private void initializeFirebase() {
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            if (mFirebaseUser.getPhotoUrl() != null) {
+                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            }
+        }
+    } // end initializeFirebase method
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
